0b477cc1e0aa994ce27662de2a2c292092e26806
vadmorda <vadmorda@gmail.com>
2026-02-10 18:12:57 +0100

    Update router.js

--- js/router.js
+++ js/router.js
@@ -29,5 +29,17 @@ export function canEnter(scene, state){
   }
   return true;
 }
+export function canEnter(scene, state){
+  if(scene.id === "cut_r1_r2" || scene.id === "r2_printshop" || scene.id === "r2_success"){
+    return !!state.completed.seal1;
+  }
+  if(scene.id === "cut_r2_r3" || scene.id === "r3_gallery" || scene.id === "r3_success"){
+    return !!state.completed.seal2;
+  }
+  if(scene.id === "cut_r3_r4" || scene.id === "r4_maproom" || scene.id === "r4_success"){
+    return !!state.completed.seal3;
+  }
+  return true;
+}
 
 
